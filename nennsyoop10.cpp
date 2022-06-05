4c2eb29170733673f4144385233b22a58a58dcd5
Karina Lavrenieva <lkvlkvlkv2003@yandex.ru>
2022-06-06 01:50:28 +0300

    Добавьте файлы проекта.

---
+++ nennsyoop10.cpp
@@ -0,0 +1,26 @@
+﻿#include "abstract.h"
+
+
+int main() {
+
+	Student s1(
+		"Ivan",
+		"Ivan",
+		"Ivanovich",
+		22,
+		1
+	);
+	
+
+	Boss b1(
+		"Ivan",
+		"Ivan",
+		"Ivanovich",
+		22,
+		1000
+	);
+	s1.print();
+	b1.print();
+
+	return 0;
+}
